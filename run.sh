64587d404b5a847934a01375f32668e692fe98c9
Arthur Barata <arthurpbarata@gmail.com>
2026-01-29 15:22:33 -0300

    update balancing

--- run.sh
+++ run.sh
@@ -1,14 +1,12 @@
-docker build -t matmul-flask:latest .
+# docker build -t matmul-flask:latest .
 
 echo "Pushing image into local repository..."
-docker tag matmul-flask:latest localhost:5000/matmul-flask:latest
-docker push localhost:5000/matmul-flask:latest
+# docker tag matmul-flask:latest localhost:5000/matmul-flask:latest
+# docker push localhost:5000/matmul-flask:latest
 
 echo "Veryfying deploied image..."
 curl -X GET http://localhost:5000/v2/_catalog
 
 echo "Applying Kubernetes manifest..."
+kubectl apply -f hpa.yaml
 kubectl apply -f manifest.yaml
-
-sleep 10
-kubectl port-forward service/matmul-flask-app-service 5050:5050
