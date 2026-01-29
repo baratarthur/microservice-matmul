af6009ca54835d2f01d8fc46a8825a01a2f933c2
Arthur Barata <arthurpbarata@gmail.com>
2026-01-29 08:51:15 -0300

    change docker to push in local registry

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 docker build -t matmul-flask:latest .
 
-minikube image rm matmul-flask:latest
-echo "Loading image into minikube..."
-minikube image load matmul-flask:latest
+echo "Pushing image into local repository..."
+docker tag matmul-flask:latest localhost:5000/matmul-flask:latest
+docker push localhost:5000/matmul-flask:latest
+
+echo "Veryfying deploied image..."
+curl -X GET http://localhost:5000/v2/_catalog
 
 echo "Applying Kubernetes manifest..."
 kubectl apply -f manifest.yaml
