5c9030fb5bb8af31f0d35537d3351718136c1242
Arthur Barata <arthurpbarata@gmail.com>
2026-02-03 15:01:32 -0300

    add toml file

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
-# docker build -t matmul-flask:latest .
+docker buildx create --use --config buildkitd.toml
+docker buildx build --platform linux/amd64,linux/arm64 -t my.private-registry.lan:5000/matmul-flask:latest --push .
 
 echo "Pushing image into local repository..."
-# docker tag matmul-flask:latest localhost:5000/matmul-flask:latest
-# docker push localhost:5000/matmul-flask:latest
+# docker tag matmul-flask:latest my.private-registry.lan:5000/matmul-flask:latest
+# docker push my.private-registry.lan:5000/matmul-flask:latest
 
 echo "Veryfying deploied image..."
-curl -X GET http://localhost:5000/v2/_catalog
+curl -X GET http://my.private-registry.lan:5000/v2/_catalog
 
 echo "Applying Kubernetes manifest..."
 kubectl apply -f hpa.yaml
